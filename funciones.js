19a00b96e812227edcea192139d8b1f0049ac407
adrianllalli <58235657+adrianllalli@users.noreply.github.com>
2022-11-07 19:35:54 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,27 @@
+function datos1() {
+    document.getElementById('nombre').innerHTML = 'Adrían';
+    document.getElementById('apellidos').innerHTML = 'Llalli Pérez';
+    document.getElementById('celular').innerHTML = '916056564';
+    document.getElementById('email').innerHTML = 'adrianllalli@gmail.com';
+}
+
+function datos2() {
+    document.getElementById('nombre').innerHTML = 'Alexander Van';
+    document.getElementById('apellidos').innerHTML = 'Wisky Perez';
+    document.getElementById('celular').innerHTML = '947363130';
+    document.getElementById('email').innerHTML = 'alew1407@gmail.com';
+}
+
+function datos3() {
+    document.getElementById('nombre').innerHTML = 'Cristhian Johan';
+    document.getElementById('apellidos').innerHTML = 'Valdivia Salas';
+    document.getElementById('celular').innerHTML = '988525501';
+    document.getElementById('email').innerHTML = 'crisvaldivia1093@gmail.com';
+}
+
+function datos4() {
+    document.getElementById('nombre').innerHTML = 'Bryan Alex';
+    document.getElementById('apellidos').innerHTML = 'López Breña';
+    document.getElementById('celular').innerHTML = '933555540';
+    document.getElementById('email').innerHTML = 'yo-u1@hotmail.com';
+}
